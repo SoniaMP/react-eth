e3153fb62454c5f10f2e0762d8daa50f701e2834
molison <sonia.molina@innobadev.altia.es>
2025-06-04 16:39:08 +0200

    [-]: Connect MetaMask and see balance

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,19 +1,62 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
+const { ethereum } = window as any;
 
 export const Balance = () => {
+  const [account, setAccount] = useState<string | null>(null);
+  const [balance, setBalance] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (ethereum) {
+      ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts: string[]) => {
+          if (accounts.length > 0) {
+            setAccount(accounts[0].toString());
+            ethereum.on("accountsChanged", (accounts: string[]) => {
+              if (accounts.length > 0) {
+                setAccount(accounts[0]);
+              }
+            });
+          }
+        });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (account) {
+      const provider = new ethers.BrowserProvider(ethereum);
+      provider.getBalance(account).then((balance: bigint) => {
+        setBalance(ethers.formatEther(balance));
+      });
+    }
+  }, [account]);
+
+  if (!ethereum) {
+    return (
+      <Typography>Please install MetaMask to view your balance.</Typography>
+    );
+  }
+
+  if (!account) {
+    return <Typography>Connecting to your wallet...</Typography>;
+  }
+
   return (
     <Box
       mt={2}
       p={2}
       textAlign="center"
-      style={{
-        backgroundColor: "#FAF8FD",
-        borderRadius: "8px",
-      }}
+      style={{ backgroundColor: "#FAF8FD", borderRadius: 8 }}
     >
-      <h2>Balance</h2>
-      <p>Your current balance is:</p>
-      <h3>€100.00</h3>
+      <Typography variant="h2">Balance</Typography>
+      <Typography>
+        Account: <strong>{account}</strong>
+      </Typography>
+      <Typography>
+        Current balance: <strong>{balance}</strong>
+      </Typography>
     </Box>
   );
 };
