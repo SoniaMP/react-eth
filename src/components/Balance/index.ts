8e961b1dc7a6070966616d8e64dc3d6d7ca2f009
Sonia Molina <socsonhja@gmail.com>
2025-06-05 18:29:16 +0200

    [-]: Overpass mock server
    Co-authored-by: molison <sonia.molina@innobadev.altia.es>

--- src/components/Balance/index.ts
+++ src/components/Balance/index.ts
@@ -1,13 +1,1 @@
-/*
- * .
- *        . * .
- *      * RRRR  *   Copyright (c) 2012 - 2025
- *     .  RR  R  .  EUIPO - European Union Intellectual Property Office
- *     *  RRR    *
- *      . RR RR .   ALL RIGHTS RESERVED
- *       *. _ .*
- * .
- *  The use and distribution of this software is under the restrictions exposed in 'license.txt'
- */
-
 export { Balance } from "./Balance";
