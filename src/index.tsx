d265ea7ba2f5cfb96b021a59075c7bc38d572721
molison <sonia.molina@innobadev.altia.es>
2025-06-05 18:29:49 +0200

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -20,6 +20,7 @@ createRoot(document.getElementById("root")!).render(
             queries: {
               refetchOnWindowFocus: false,
               refetchOnReconnect: false,
+              retry: false,
             },
           },
         })
